41457de4129f01ad62b153732a7ef8c389439013
Song Yang <songyang1218@gmail.com>
2022-02-19 21:55:39 -0500

    ✨ Mobile navigation bar icons

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -1,3 +1,35 @@
 <template>
-  <div>Test</div>
+  <div class="d-md-none">
+    <div class="container-fluid m-0 p-0">
+      <div class="row">
+        <div class="position relative">
+          <img
+            class="nav-icon position-absolute top-0 start-0"
+            :src="require('@/assets/logo mobile.svg')"
+            alt="Logo"
+          />
+          <img
+            class="nav-icon position-absolute top-0 end-0"
+            :src="require('@/assets/menu icon.svg')"
+            alt="Menu button"
+          />
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="d-none d-md-block">DESKTOP VERSION</div>
 </template>
+
+<style lang="scss">
+.nav-icon {
+  width: 61px;
+  height: 61px;
+}
+
+#logo {
+  position: absolute;
+  left: 0px;
+  top: 0px;
+}
+</style>
