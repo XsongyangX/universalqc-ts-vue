68d55bf50a02f97654532a382cef7ee723e6f60e
Song Yang <songyang1218@gmail.com>
2022-02-25 01:03:02 -0500

    ✨ Montreal background

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,18 @@
 <template>
-  <div class="home"></div>
+  <div class="container-fluid p-0">
+    <img class="background" :src="require('@/assets/montreal-hero.jpg')" alt="Montreal background" />
+  </div>
 </template>
+
+<style lang="scss">
+.background {
+  height: 640px;
+  max-width: 100%;
+  min-height: 100%;
+  object-fit: cover;
+  object-position: 50% 100%;
+}
+</style>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
