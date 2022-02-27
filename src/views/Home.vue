10f44515a2928667bb7e88e5965e735618c29b32
Song Yang <songyang1218@gmail.com>
2022-02-27 12:23:46 -0500

    ✨ Stretch the background

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,28 @@
 <template>
-  <div class="container-fluid p-0">
-    <img class="background" :src="require('@/assets/montreal-hero.jpg')" alt="Montreal background" />
+  <div class="container-fluid p-0 m-0">
+    <img
+      class="img-responsive background"
+      :src="require('@/assets/montreal-hero.jpg')"
+      alt="Montreal background"
+    />
   </div>
 </template>
 
 <style lang="scss">
+// in case you need the bootstrap breakpoint vars
+// @import "~bootstrap/scss/_functions.scss";
+// @import "~bootstrap/scss/_variables.scss";
+// @import "~bootstrap/scss/mixins/_breakpoints.scss";
+
 .background {
-  height: 640px;
   max-width: 100%;
   min-height: 100%;
   object-fit: cover;
   object-position: 50% 100%;
+  // below this width, we crop the image and only see a part of it
+  @media only screen and (max-width: 1080px) {
+    height: 640px;
+  }
 }
 </style>
 
