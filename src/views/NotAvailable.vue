bd610577a21200584e2d9a2b29dff581929f0225
Song Yang <songyang1218@gmail.com>
2022-04-03 11:27:52 -0400

    ✨ Error redirection page

---
+++ src/views/NotAvailable.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="about-page">
+        <div class="page-header">
+            <p class="page-header-text">PAGE NOT AVAILABLE</p>
+        </div>
+        <div class="page-description-box">
+            <p class="page-description-text">
+                The page you clicked on is still not available for viewing!
+                <br />
+                <br />Or you may have clicked on a broken link.
+            </p>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@import "@/scss/about-page.scss";
+</style>
