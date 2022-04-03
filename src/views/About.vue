6502a31cb80661608ad63f440d7465c7b88b9528
Song Yang <songyang1218@gmail.com>
2022-04-02 20:02:44 -0400

    ✨ About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,67 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about-page">
+    <!-- Purple about header -->
+    <div class="page-header">
+      <p class="page-header-text">ABOUT</p>
+    </div>
+    <!-- Description -->
+    <div class="page-description-box">
+      <p class="page-description-text">
+        The Universal Party is a provincial political party in Quebec, Canada. The idea for the party started in 2020 by Song Yang.
+        <br />
+        <br />The party aims to be a full-on party that tackles all issues from a consistent ideology centered around individual rights and a left-of-center policy guideline.
+        <br />
+        <br />Although inspired by the late Equality Party, the Universal Party is not a one-issue party nor a Liberal protest party. We will never merge with the Liberal establishment.
+      </p>
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+.about-page {
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+}
+
+.page-header {
+  height: 112px;
+
+  margin-top: 61px;
+
+  background: rgba(143, 96, 220, 0.85);
+
+  display: flex;
+}
+
+.page-header-text {
+  font-family: "Open Sans";
+  font-style: normal;
+  font-weight: 700;
+  font-size: 26px;
+  line-height: 35px;
+  display: flex;
+  align-items: center;
+  text-align: center;
+
+  margin: auto;
+
+  color: #ffffff;
+}
+
+.page-description-box {
+  background: rgba(255, 255, 255, 0.89);
+  padding: 5%;
+}
+
+.page-description-text {
+  font-family: "Open Sans";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 20px;
+  line-height: 27px;
+
+  color: #000000;
+}
+</style>
