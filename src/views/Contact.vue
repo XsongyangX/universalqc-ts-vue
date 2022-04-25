5457b332cc8f6c814fb50a02963436c3aa2a5756
Song Yang <songyang1218@gmail.com>
2022-04-24 22:26:30 -0400

    ✨ Social media links

--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -17,6 +17,23 @@
                 <a href="mailto:songyang.politics@gmail.com">songyang.politics@gmail.com</a>
             </p>
         </div>
+        <div class="page-header">
+            <p class="page-header-text">SOCIAL MEDIA</p>
+        </div>
+        <div class="page-description-box">
+            <a href="https://www.facebook.com/universalQC" target="_blank"><img style="margin: 10px"
+                    :src="require('@/assets/facebook icon.svg')" alt="Facebook"></a>
+            <a href="https://www.youtube.com/channel/UCT5jlPJQS4DfP8Ugo2_dwoQ" target="_blank"><img style="margin: 10px"
+                    :src="require('@/assets/youtube icon.svg')" alt="YouTube"></a>
+            <a href="https://www.tiktok.com/@universalqc" target="_blank"><img style="margin: 10px"
+                    :src="require('@/assets/tiktok icon.svg')" alt="TikTok"></a>
+            <a href="https://twitter.com/UnivPartyQC" target="_blank"><img style="margin: 10px"
+                    :src="require('@/assets/twitter icon.svg')" alt="Twitter"></a>
+            <a href="https://www.instagram.com/universalpartyqc/" target="_blank"><img style="margin: 10px"
+                    :src="require('@/assets/instagram icon.svg')" alt="Instagram"></a>
+            <a href="https://medium.com/@songyang-politics" target="_blank"><img style="margin: 10px"
+                    :src="require('@/assets/medium icon.svg')" alt="Medium"></a>
+        </div>
     </div>
 </template>
 
